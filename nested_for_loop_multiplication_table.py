6ba685be92ff408286df59cb5389a58e6f834584
richiemelcabantog22 <cabantogrichiemel_bsit@plmun.edu.ph>
2025-02-26 13:48:30 +0800

    Submitting Python Week 04 - Laboratory # 14

--- nested_for_loop_multiplication_table.py
+++ nested_for_loop_multiplication_table.py
@@ -1,5 +1,5 @@
 for i in range(1, 6):
-  for j in range(1, 6):  # Indent this loop!
-	  product = i * j  # Indent this line!
-	  print(f"{product:4}", end="")  # Indent this line!
-	  print() # Indent this line!
+    for j in range(1, 6):  # Indent this loop!
+        product = i * j  # Indent this line!
+        print(f"{product:4}", end="")  # Indent this line!
+    print()  # Indent this line!
